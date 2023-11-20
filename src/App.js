12eda33dae14d3595f8332da3a1838ed40825acb
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-20 16:45:40 +0000

    Contact page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import About from './components/About';
 import Login from './components/Login';
 import Register from './components/Register'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer/>
     </div>

171a6db87b44379533cd2f64e528d0bb47b351a0
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-20 16:38:50 +0000

    Footer implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }

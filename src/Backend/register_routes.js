da44e1e135a65e0dc12db3ca3bcab98589770093
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-26 22:28:06 +0000

    Error message for duplicate users

--- src/Backend/register_routes.js
+++ src/Backend/register_routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoError } from 'mongodb';
 
 const registerRoutes = (RegisterModel) => {
   const router = express.Router();
@@ -12,12 +13,20 @@ const registerRoutes = (RegisterModel) => {
       }
 
       // Wait for the create method to complete before sending the success message
-      await RegisterModel.create({ username, email, password, confirmPassword });
+      const newUser = await RegisterModel.create({ username, email, password, confirmPassword });
 
-      console.log('User saved to DB!');
+      console.log('User saved to DB:', newUser);
+
       return res.status(201).json({ message: 'User saved to DB!' });
     } catch (error) {
       console.error(error);
+
+      if (error instanceof MongoError && error.code === 11000) {
+        // Duplicate key error (e.g., duplicate email or username)
+        console.error('Duplicate key error. User already exists:', error.message);
+        return res.status(400).json({ message: 'Duplicate key error. User already exists.' });
+      }
+
       return res.status(500).json({ message: error.message || 'Server error', error });
     }
   });
